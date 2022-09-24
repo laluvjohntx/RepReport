da7ddde586acc6cd22496daf107c611d0c04dc49
Lalu John <lalujohn@lalus-mbp.event.rightround.com>
2022-09-24 09:03:05 -0500

    LOOK AT ME MA, I GOT MONGO WORKING

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient;
+
 
 const staffRoutes = require('./routes/staffRoutes.js')
 const patientRoutes = require('./routes/patientRoutes.js')
@@ -11,16 +13,32 @@ app.use("/staff", staffRoutes)
 app.use("/patient", patientRoutes)
 app.use("/inventory", inventoryRoutes)
 
+const url = 'mongodb://127.0.0.1:27017';
+
+
 const start = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb://root:example@mongo:27017/"
-    );
+  // try {
+  //   await mongoose.connect(
+  //     "mongodb://root:example@mongo:27017/"
+  //   );
+  //   app.listen(3000, () => console.log("Server started on port 3000"));
+  // } catch (error) {
+  //   console.error(error);
+  //   process.exit(1);
+  // }
+  MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    // Specify database you want to access
+    const db = client.db('PATIENT');
     app.listen(3000, () => console.log("Server started on port 3000"));
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+    console.log(`MongoDB Connected: ${url}`);
+});
 };
 
 start();
